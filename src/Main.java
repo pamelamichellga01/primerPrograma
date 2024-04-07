1e9c2b32bd8055fbe1e97beb33ddc27dfb952dcd
pamelamichellga01 <72150148+pamelamichellga01@users.noreply.github.com>
2024-04-07 14:23:54 -0500

    avances en el proyecto de java 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
-public class Main {
+;public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Bienvenido(a) a Screen Match");
+        System.out.println("Pelicula: Matrix");
+
+        int fechaDeLanzamiento = 1999;
+        boolean IncluidoEnElPlan = true;
+        double notaDeLaPelicula = 8.2;
+
+        double media = (8.2 + 6.0 + 9.0 )/3;
+        System.out.println(media);
+
     }
 }
