9e792979a8faf21a306241334137bf690811265f
pamelamichellga01 <72150148+pamelamichellga01@users.noreply.github.com>
2024-04-07 14:32:27 -0500

    avances en el proyecto de java 1 en otra rama

--- src/Main.java
+++ src/Main.java
@@ -10,5 +10,13 @@
         double media = (8.2 + 6.0 + 9.0 )/3;
         System.out.println(media);
 
+        String sinopsis = """
+                Matrix es una paradoja 
+                La mejor pelicula del din del milenio
+                Fue lanzada en: 
+                """ + fechaDeLanzamiento;
+
+        System.out.println(sinopsis);
+
     }
 }
