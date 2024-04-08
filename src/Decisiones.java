b764d68c27a060a20e9ac7cd9ff834ee5139e156
pamelamichellga01 <72150148+pamelamichellga01@users.noreply.github.com>
2024-04-07 20:09:35 -0500

    actualizaciones de las actividades para aula seis

---
+++ src/Decisiones.java
@@ -0,0 +1,13 @@
+public class Decisiones {
+    public static void main(String[] args) {
+        int fechaDeLanzamiento = 1999;
+        boolean IncluidoEnElPlan = true;
+        double notaDeLaPelicula = 8.2;
+
+        if (fechaDeLanzamiento >= 2022){
+            System.out.println("Peliculas mas populares");
+        }else{
+            System.out.println("Peliculas retro que aun vale la pena ver");
+        }
+    }
+}
