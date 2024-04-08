024b93ce66fea9f80301032ea148acefd2a2da3e
pamelamichellga01 <72150148+pamelamichellga01@users.noreply.github.com>
2024-04-07 20:20:50 -0500

    actualizaciones de las actividades para aula siete

--- src/Decisiones.java
+++ src/Decisiones.java
@@ -1,13 +1,20 @@
 public class Decisiones {
     public static void main(String[] args) {
         int fechaDeLanzamiento = 1999;
-        boolean IncluidoEnElPlan = true;
+        boolean incluidoEnElPlan = true;
         double notaDeLaPelicula = 8.2;
+        String tipoPlan = "Plus";
 
         if (fechaDeLanzamiento >= 2022){
             System.out.println("Peliculas mas populares");
         }else{
             System.out.println("Peliculas retro que aun vale la pena ver");
+        }
+
+        if (incluidoEnElPlan && tipoPlan.equals("Plus")){
+            System.out.println("Disfrute de su pelicula");
+        }else {
+            System.out.println("Pelicula no disponible para su plan actual");
         }
     }
 }
